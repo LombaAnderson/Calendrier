c9736be0160175456364467fd651d9145f082b44
Anderson Lomba <andersonlomba172@gmail.com>
2020-08-05 10:18:17 -0300

    Calendrier de l'année 2020 fait avec html, css et le language Javascript

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+
+<html>
+
+<head>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body>
+
+<nav class="navbar navbar-dark bg-primary mb-3">
+	<a href="index.php" class="navbar-brand">Mon calendrier</a>
+
+</nav>
+
+	<h1>Mai 2020</h1>
+
+
+
+</body>
+
+
+
+</hmtl>
